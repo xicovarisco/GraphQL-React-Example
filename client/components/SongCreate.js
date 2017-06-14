f09de97d9234e2555eb1e8c4a74c58cb9f38263c
Francisco Varisco <francisco.varisco@gmail.com>
2017-06-15 09:15:14 +1000

    Song Creation page

---
+++ client/components/SongCreate.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import gql from 'graphql-tag';
+
+class SongCreate extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = { title: '' }
+    }
+
+    onSubmit(event) {
+        event.preventDefault();
+
+
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <h3>Create a New Song</h3>
+                <form onSubmit={this.onSubmit.bind(this)}>
+                    <label>Song Title:</label>
+                    <input
+                        onChange={event => this.setState({ title: event.target.value })}
+                        value={this.state.title}
+                    />
+                </form>
+            </div>
+        )
+    }
+}
+
+const mutation = gql`
+    mutation {
+        addSong(title:"Dog Call") {
+            id
+            title
+        }
+    }
+`;
+
+export default SongCreate;
