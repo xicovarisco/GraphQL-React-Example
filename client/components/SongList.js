6cca9e425709e0889c3400acb84f6b05cbd7f63c
Francisco Varisco <francisco.varisco@gmail.com>
2017-06-15 09:15:02 +1000

    List of Songs with gql tag

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,11 +1,39 @@
 import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
 
 class SongList extends Component {
+
+    renderSongs() {
+        return this.props.data.songs.map(song => {
+            return (
+                <li key={song.id} className="collection-item">
+                    {song.title}
+                </li>
+            )
+        })
+    }
+
     render() {
+
+        console.log(this.props);
+        if (this.props.data.loading) { return <div>Loading...</div> }
+
         return (
-            <div>
-                SongList
-            </div>
+            <ul className="collection">
+                {this.renderSongs()}
+            </ul>
         )
     }
 }
+
+const query = gql`
+    {
+        songs {
+            id
+            title
+        }
+    }
+`;
+
+export default graphql(query)(SongList);
