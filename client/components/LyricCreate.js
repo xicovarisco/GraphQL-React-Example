640bfd9f030c42e7c6e7e8d304d9f27db34feca6
Francisco Varisco <francisco.varisco@gmail.com>
2017-07-17 09:18:42 +1000

    Creation of Lyrics

---
+++ client/components/LyricCreate.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
+
+class LyricCreate extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = { content: '' };;
+    }
+
+    onSubmit(event) {
+        event.preventDefault();
+
+        // this.props.mutate({
+        //     variables: { songId: this.state.title },
+        //     refetchQueries: [{ query: query }]
+        // }).then(() => hashHistory.push('/'));
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.onSubmit.bind(this)}>
+                <label>Add a Lyric</label>
+                <input
+                    value={this.state.content}
+                    onChange={event => this.setState({content: event.target.value })}
+                />
+            </form>
+        )
+    }
+}
+
+const mutation = gql`
+    mutation AddLyricToSong($songId: ID, $content: String){
+        addLyricToSong(songId: $songId, content: $content) {
+            id
+            lyrics {
+                content
+            }
+        }
+    }
+`;
+
+export default graphql(mutation)(LyricCreate);
