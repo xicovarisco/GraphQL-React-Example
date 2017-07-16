6dd48e5bb9d6c1d873db5c76db741a38c397bff4
Francisco Varisco <francisco.varisco@gmail.com>
2017-07-17 09:18:19 +1000

    New Song Details Window

---
+++ client/components/SongDetail.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import { Link } from 'react-router';
+import fetchSong from '../queries/fetchSong';
+import LyricCreate from './LyricCreate';
+
+class SongDetail extends Component {
+
+    render() {
+
+        const { song } = this.props.data;
+
+        if (!song) {
+            return <div></div>
+        }
+
+        return (
+            <div>
+                <Link to="/">Back</Link>
+                <h3>{song.title}</h3>
+                <LyricCreate />
+            </div>
+        )
+    }
+}
+
+export default graphql(fetchSong, {
+    options: (props) => { return { variables: { id: props.params.id } } }
+})(SongDetail);
