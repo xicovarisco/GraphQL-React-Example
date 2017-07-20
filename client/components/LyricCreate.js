f7e7dd9aae5ec200b1fd40d87658f6a629d6b59f
Francisco Varisco <francisco.varisco@gmail.com>
2017-07-20 19:25:52 +1000

    Like Lyric

--- client/components/LyricCreate.js
+++ client/components/LyricCreate.js
@@ -39,7 +39,9 @@ const mutation = gql`
         addLyricToSong(songId: $songId, content: $content) {
             id
             lyrics {
+                id
                 content
+                likes
             }
         }
     }
--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -1,12 +1,37 @@
 import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
 
 class LyricList extends Component {
+
+    onLike(id, likes) {
+        this.props.mutate({
+            variables: { id },
+            optimisticResponse: {
+                _typename: 'Mutation',
+                likeLyric: {
+                    id: id,
+                    __typename: 'LyricType',
+                    likes: likes + 1
+                }
+            }
+        });
+    }
 
     renderLyrics() {
         return this.props.lyrics.map(lyric => {
             return (
                 <li key={lyric.id} className="collection-item">
                     {lyric.content}
+                    <div className="vote-box">
+                        <i
+                            className="material-icons"
+                            onClick={() => this.onLike(lyric.id, lyric.likes)}
+                        >
+                            thumb_up
+                        </i>
+                        {lyric.likes}
+                    </div>
                 </li>
             )
         })
@@ -21,4 +46,13 @@ class LyricList extends Component {
     }
 }
 
-export default LyricList;
+const mutation = gql`
+    mutation LikeLyric($id: ID) {
+        likeLyric(id: $id){
+            id
+            likes
+        }
+    }
+`
+
+export default graphql(mutation)(LyricList);
